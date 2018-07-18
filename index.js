d7ca002e1e892513319fba6741c63fc627c6b03e
jiangxiaodong <jiangxiaodong@100tal.com>
2018-07-18 13:12:20 +0800

    .

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 let htmlStr='<div class="toolbar"><span><a href="#" class="toolbar_item command_help help">?</a></span></div>';
+
+//匹配一个html标签开头部分
+const matchBeginningElement=/^<(.)*[^<]/;
+
+console.log(htmlStr.match(matchBeginningElement));
