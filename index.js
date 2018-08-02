f8bd4245652904dc19429dc756207d147e026c8f
jxd <1053376250@qq.com>
2018-08-02 22:36:37 +0800

    完善代码

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let htmlStr = '<div class="toolbar"><span><a href="#" class="toolbar_item command_help help">?</a></span></div>';
-let VNode=require('./vnode');
+let VNode = require('./vnode');
 //需要的正则表达式
 //拿到一个标签起始
 let startIndex = 0;
 let endIndex = 0;
-let elementStack=[];//确认元素是否闭合的栈
+let elementStack = [];//确认元素是否闭合的栈
 function htmlParse(htmlStr) {
     //做一些html清理工作，保证循环的时候是一个干净的html字符串
     if (typeof htmlStr !== 'string') {
@@ -38,10 +38,10 @@ function htmlParse(htmlStr) {
             endIndex = htmlStr.indexOf('<');
             endIndex = htmlStr.lastIndexOf('>', endIndex);
             //找到一个完整的字符串头
-             parseHtmlElement(htmlStr.slice(startIndex, endIndex))
-             //截取字符串
+            parseHtmlElement(htmlStr.slice(startIndex, endIndex))
+            //截取字符串
 
-             htmlStr=htmlStr(endIndex+1);
+            htmlStr = htmlStr(endIndex + 1);
         }
 
 
@@ -49,49 +49,55 @@ function htmlParse(htmlStr) {
 }
 // 
 // const matchElementNameAttribute=/(\w+)\s*(?:\s*((\w+)\s*(=)\s*("([^<>\s""]*)"|([^<>\s""]*)|'([^<>\s'']*)')))/
-const attriBute=/\s*((\w+)\s*(=)\s*("([^<>\s""]*)"|([^<>\s""]*)|'([^<>\s'']*)'))/
-const cname=/\s*(\/?)\s*(\w+)/
-function parseHtmlElement(htmlStr){
+const attriBute = /\s*((\w+)\s*(=)\s*("([^<>\s""]*)"|([^<>\s""]*)|'([^<>\s'']*)'))/
+const cname = /\s*(\/?)\s*(\w+)/
+function parseHtmlElement(htmlStr) {
     //正则表达式匹配元素名称属性的key  value
     // div class="toolbar"
     //匹配元素名称
-    let matchElementName= htmlStr.match(cname);
-    if(!matchElementName){
+    let matchElementName = htmlStr.match(cname);
+    if (!matchElementName) {
         return;
     }
-    let elementName=matchElementName[2];
-    let endTag=matchElementName[1];
-    if(typeof elementName!=='string'){
+    let elementName = matchElementName[2];
+    let endTag = matchElementName[1];
+    if (typeof elementName !== 'string') {
         return;
     }
-    elementName=elementName.toUpperCase();
-    if(endTag){
+    elementName = elementName.toUpperCase();
+    let start = elementStack[elementStack.length];
+    let vNode = null;
+    if (endTag) {
         //上一个元素名称如果不相同忽略此次匹配
-        if(elementStack.length==0){
+        if (elementStack.length == 0) {
             return;
         }
-        let start=elementStack[elementStack.length];
-        let vNode=null;
-        if(start.name===elementName){
-            elementStack.pop();
-        }else{
-            vNode=new VNode(elementName,1);
-            vNode.parentNode=start;
-            elementStack.push(vNode);
+       
+       
+        //如果闭合出栈
+        if (start.name !== elementName) {
+            return;
         }
-
-        
+        elementStack.pop();
+        return;
     }
 
+    //
+     //dom结点
+     vNode = new VNode(elementName, 1);
+     vNode.parentNode = start;
+     start.addChildren(vNode)
+     if (!vNode.isSelfClosure) {
+         elementStack.push(vNode);
+     }
 
-    let vNode=new VNode(elementName,1);
-    if(!vNode.isSelfClosure){
-
-    }
 
 }
 
-function parseAttrbute(attrStr){
+
+}
+
+function parseAttrbute(attrStr) {
 
 }
 
